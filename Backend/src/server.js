2fbe9bc41ef9057de89302fd8ba220f125179848
belgianwafflecorp <domien.verstraete@student.vives.be>
2025-02-11 13:38:21 +0100

    Add dotenv for environment variable management and update server port configuration

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,12 +1,15 @@
-import express from 'express';      
-const app = express();
+import express from 'express'; 
+import dotenv from 'dotenv';
 
-const port = 3000
+const app = express(); // Create an Express app
+dotenv.config() // Load environment variables from .env file
+
+const port = process.env.DB_PORT;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`vives-card-game-backend app listening on port ${port}`)
 })
