9f41c4978722b6b296817a40df7db881ee1d57d5
Domien Verstraete <verstraeted@protonmail.com>
2025-02-13 15:22:47 +0100

    refactor: update database connection to remove default empty password

--- Backend/src/db.js
+++ Backend/src/db.js
@@ -5,7 +5,7 @@ import mysql from 'mysql2/promise';
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS || '',  // Zorg ervoor dat een lege string wordt gebruikt als er geen wachtwoord is
+    password: process.env.DB_PASS,  
     database: process.env.DB_DATABASE,
     waitForConnections: true,
     connectionLimit: 10
