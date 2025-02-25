329ac0b2a31c9ba5701f25ca06fb34ed2b9722a8
belgianwafflecorp <domien.verstraete@student.vives.be>
2025-02-25 12:42:17 +0100

    Refactor user routes to improve organization and remove duplicate endpoints

--- Backend/src/routes/userRoutes.js
+++ Backend/src/routes/userRoutes.js
@@ -2,19 +2,25 @@ import express from 'express';
 const router = express.Router();
 import * as UserController from '../controllers/UserController.js';
 
+// users
 router.post('/addUser', UserController.addUser);
+router.post('/register', UserController.registerUser);
+router.post('/login', UserController.loginUser);
 router.get('/getAllUsers', UserController.getAllUsers);
 router.get('/getUserBy/:param/:value', UserController.getUserBy);
 router.delete('/deleteUser/:id', UserController.deleteUser);
+
+// leaderboards
 router.get('/leaderboard/weekly', UserController.getWeeklyLeaderboard);
 router.get('/leaderboard/total', UserController.getTotalLeaderboard);
 router.get('/leaderboard/study', UserController.getStudyLeaderboard);
 router.get('/leaderboard/user/:userId', UserController.getUserRanking);
+
+// cards
 router.post('/add', UserController.addCardToUser);   
 router.get('/user/:user_id', UserController.getUserCards);  
 router.post('/trade', UserController.tradeCards);  
-router.post('/register', UserController.registerUser);
-router.post('/login', UserController.loginUser);
 router.post('/starter-pack', UserController.giveStarterPack);
+
 export default router;
 
