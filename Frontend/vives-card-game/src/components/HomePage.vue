9ea3d8a105941fddd9f7ec08cc87598d2f0bcb11
Arthur Brassaert <abrassaert@gmail.com>
2025-02-27 15:26:05 +0100

    Homepage translated to dutch, battle explanation commented for now

--- Frontend/vives-card-game/src/components/HomePage.vue
+++ Frontend/vives-card-game/src/components/HomePage.vue
@@ -1,82 +1,92 @@
 <template>
     <div>
-      <h2>How to play</h2>
-      <h3>Collecting:</h3>
+      <h2>Hoe te spelen</h2>
+      <h3>Verzamelen:</h3>
       <p>
-        Each new user gets a pack of 9 cards when they create an account. From then you can get 3 free cards, per day, by just scanning a QR-code.
-        <br> Cards can also be upgraded to a higher rarity, this can be done by combining 3 of the same card, this will give you 1 card with a higher rarity.
+        Elke gebruiker krijgt een pack met 9 kaarten als ze een account aanmaken. Daarna kun je dagelijks 3 kaarten claimen door simpelweg in te loggen.
+        <br> Je kan ook kaarten upgraden naar een hogere zeldzaamheid, dit kan door 3 dezelfde kaarten te combineren, dit geeft je 1 kaart met een hogere zeldzaamheid.
       </p>
-      <h3>Trading:</h3>
+      <h3>Ruilen:</h3>
       <p>
         There's a trading system, made to interact with other students on the campus. You can trade cards, only if you're in the same GPS-location.
         <br> To start the trade, one player will be able to generate a QR-code that can be scanned by the other player. If both players confirm the trade, the cards will be transfered.
       </p>
-      <h3>Battling:</h3>
+      <!--<h3>Vechten:</h3>
       <p>
         Battles will be played in a 1v1 format, where each player can use 3 cards. The order in which the players start will be decided by a coin flip.
         <br> The player that wins the coin flip will start the battle and can perform one of the three options: attack (deals damage to the other player's active card), use ability (uses the ability of the active card) and switch (gives you the option to switch your active card with an inactive one). After that each player can perform 2 actions per turn.
         <br> If you can kill all the cards of your opponent, you win the battle and gain yourself the total amount of points of the cards you defeated. 
         <br> The loser also gets some points, they get the amount of points from the lowest rarity card of the opponent.
 
-      </p>
+      </p>-->
       
 
 
-      <h2>Leaderboard</h2>
+      <h2>Scorebord</h2>
       <p>
-        The leaderboard will be split up into 3 different sections: group leaderboard, individual leaderboard and in the third section a QR-code for the daily login. 
+        Het scorebord zal opgedeeld worden in 3 secties: groepsscorebord, individueel scorebord en in de laatste sectie komt een QR-code.
       </p>
-      <h3>Group leaderboard:</h3>
+      <h3>Groepsscorebord:</h3>
       <p>
-        The group leaderboard shows you the total amount of points scored by all the members of each field of study. 
-        <br>(For example: Technology - 3000pts, Business - 2500pts, ...)
+        Het groepsscorebord zal weergeven hoeveel punten alle studenten van een specifiek studiegebied gescoord hebben. 
+        <br>(Bijvoorbeeld: Technology - 3000ptn, Handel & Business - 2500ptn, ...)
       </p>
-      <h3>Individual leaderboard:</h3>
+      <h3>Individueel scorebord:</h3>
       <p>
-        The individual leaderboard shows you the total amount of points scored by each individual user.
+        Het individuele scorebord zal de ranking van alle individuele spelers tonen.
       </p>
-      <h3>Daily login:</h3>
+      <h3>Dagelijkse login:</h3>
       <p>
-        In this section of the board we will display a QR-code for quick access tot the login page, where you can claim the daily reward.
+        In de laatste sectie kun je via de QR-code direct naar de login-pagina gaan, hiermee kun je makkelijk je dagelijkse beloning claimen.
       </p>
 
 
 
-      <h2>The cards</h2>
-      <p> Each card will have the following info on them:</p>
-      <ul>
-        <li>Card name (center title)</li>
-        <li>Artwork (illustration of the card)</li>
-        <li>Card rarity (border around the card)</li>
-        <li>Card type (left circle) </li>
-        <li>Card subtype (right circle)</li>
-        <li>Hit points (under the card name)</li>
-        <li>Attack points (left square)</li>
-        <li>Ability (right square)</li>
+      <h2>De kaarten</h2>
+      <p> Elke kaart geeft volgende info weer:</p>
+      <ul class ="default list">
+        <li>Kaartnaam (centrale titel)</li>
+        <li>Artwork (illustratie)</li>
+        <li>Zeldzaamheid (kleur van de rand)</li>
+        <li>Kaarttype (cirkel linksboven) </li>
+        <li>Levenspunten (boven de kaartnaam)</li>
+        <li>Aanvalspunten (linker voorkant)</li>
+        <li>Vaardigheid (rechter vierkant)</li>
       </ul>
-      <p> The five different rarities each card can have are the following:</p>
-      <ul>
-        <li>Common (grey)</li>
-        <li>Uncommon (green)</li>
-        <li>Rare (blue)</li>
-        <li>Very rare (purple)</li>
-        <li>Ultra rare (gold)</li>
+      <p> De vijf verschillende zeldzaamheden zijn als volgt:</p>
+      <ul class ="default list">
+        <li>Common (grijs)</li>
+        <li>Uncommon (groen)</li>
+        <li>Rare (blauw)</li>
+        <li>Very rare (paars)</li>
+        <li>Ultra rare (goud)</li>
       </ul>
-      <p> And these are the possible card types:</p>
-      <ul>
-        <li>Healthcare ❤</li>
-        <li>Marketing and Business 🤝</li>
-        <li>Education and Social 🎓</li>
-        <li>Technology and Bio ⚙</li>
+      <p> En dit zijn alle mogelijke kaarttypes:</p>
+      <ul class ="default list">
+        <li>Technology en Bio ⚙</li>
+        <li>Gezondheidszorg ❤</li>
+        <li>Marketing en Business 🤝</li>
+        <li>Onderwijs en Sociaal 🎓</li>
       </ul>
 
 
 
-      <h2>About us</h2>
+      <h2>Over ons</h2>
       <p>
-        Our goal is to create a fun and interactive game for the students of VIVES. The game is made to be played on campus and the main goal is to interact with students of different fields of study.
-        <br> To accomplish our goal we are developing a card game where players can collect and trade cards with each other. The game will also have a battling system where players can battle each other to gain points.
+        Ons doel is om een leuke game te ontwikkelen voor de studenten van Vives. Het doel van het spel is om studenten van verschillende studiegebieden te verenigen en interactie te creëren.
+        <br> Om dit doel te verwezenlijken maken we een digitaal kaartspel waarin spelers kunnen verzamelen, ruilen en tegen elkaar strijden. Als de spelers strijden tegen elkaar kunnen ze nieuwe kaarten verdienen, alsook punten voor de rankings.
       </p>
+      <h3>Ons team:</h3>
+      <ul class ="default list">
+        <li>Verstraete Domien: Front/Back-end ontwikkelaar</li>
+        <li>Meuleman Joshua: Back-end/database ontwikkelaar, 3D designer (indien nodig)</li>
+        <li>Brassaert Arthur: Frontend ontwikkelaar</li>
+        <li>Pladijs Tom: Front-end ontwikkelaar, kaart ontwerper</li>
+        <li>Oyen Quentin: Spelregels en concept uitwerken</li>
+        <li>Declerq Jules: Grafisch ontwerper, sociale media beheren</li>
+        <li>De Smet Xander: Grafisch ontwerper</li>
+        <li>Degeest Bram: Robotics</li>
+      </ul>
 
     </div>
   </template>
@@ -94,5 +104,10 @@
 
   p{
     font-size:20px;
+  }
+
+  .default.list {
+    list-style-type: disc;
+    font-size: 18px;
   }
   </style>
