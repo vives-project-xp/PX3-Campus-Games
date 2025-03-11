6270c1146a2fec5d09a340b41fe0f5a31f838090
Arthur Brassaert <abrassaert@gmail.com>
2025-03-11 17:00:05 +0100

    achtergrondkleur alle pagina's aangepast, zodat er geen kleurverschil is met padding

--- Frontend/vives-card-game/src/App.vue
+++ Frontend/vives-card-game/src/App.vue
@@ -29,7 +29,7 @@ export default {
 :root {
   --primary-color: #fff;
   --secondary-color: #e60000;
-  --background-color: #f5f5f5;
+  --background-color: #fff;
   --text-color: #333;
   --border-color: #ddd;
   --box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
