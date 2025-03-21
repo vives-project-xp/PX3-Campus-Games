2ccbbdf40feba10883675e701ab226c201581972
belgianwafflecorp <domien.verstraete@student.vives.be>
2025-03-21 08:59:57 +0100

    remove unused assets and configuration files from frontend and backend

--- Backend/src/middleware/auth.js
+++
@@ -1,6 +0,0 @@
-// file for google authentification
-
-import passport from 'passport';
-import jwt from 'jsonwebtoken';
-import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
-import db from '../db.js';
--- Backend/src/middleware/validation.js
+++
@@ -1,7 +0,0 @@
-import Joi from 'joi';
-
-export const registerSchema = Joi.object({
-    username: Joi.string().min(3).max(30).required(),
-    education: Joi.string().min(3).max(30).required(),
-    password: Joi.string().min(6).required()
-});
