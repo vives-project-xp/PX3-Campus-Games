380c110c6304d9094934dca887fb511f157638b4
Arthur Brassaert <abrassaert@gmail.com>
2025-05-08 15:57:27 +0200

    added login button to homepage

--- docker/vue/src/components/HomePage.vue
+++ docker/vue/src/components/HomePage.vue
@@ -1,63 +1,66 @@
 <template>
-  <div class="logo"><img src="/logo_campus_games.jpg" alt="Vives Campus Games logo" class="logo-image"></div>
+  <div>
+    <div class="logo"><img src="/logo_campus_games.jpg" alt="Vives Campus Games logo" class="logo-image"></div>
+    
+    <div class="call-to-action">
+      <button v-if="!isLoggedIn" @click="goToLogin" class="login-button">Aanmelden</button>
+    </div>
+
     <div class="about-section">
       <h2>Over ons</h2>
       <p>
         Ons doel is om een game te ontwikkelen die alle opleidingen met elkaar kan verbinden, zodat studenten samen kunnen spelen en te verrenigen.
         <br>
         <br>
-        Om dit te bereiken, creëren we een online kaartspel dat heel gebruiksvriendelijk is. Spelers kunnen kaarten verdienen, ruilen en tegen elkaar strijden om kaarten en scores te verzamelen. 
-        Met deze score word er een scorebord gemaakt met de hoogste scores. We zorgen ervoor dat de kaarten er aantrekelijk en eenvoudig uitzien om een breed plubliek aan te spreken.
+        Om dit te bereiken, creëren we een online kaartspel dat heel gebruiksvriendelijk is. Spelers kunnen kaarten verdienen, ruilen en tegen elkaar strijden om kaarten en scores te verzamelen.
+        Met deze score word er een scorebord gemaakt met de hoogste scores. We zorgen ervoor dat de kaarten er aantrekkelijk en eenvoudig uitzien om een breed plubliek aan te spreken.
       </p>
     </div>
+    
     <div>
       <h2>Hoe te spelen</h2>
-    <button cl
-      @click="toggleContent" 
-      class="toggle-button"
-    >
-      {{ isContentVisible ? 'Verberg uitleg' : 'Toon uitleg:' }}
-    </button>
+      <button
+        @click="toggleContent"
+        class="toggle-button"
+      >
+        {{ isContentVisible ? 'Verberg uitleg' : 'Toon uitleg:' }}
+      </button>
 
-    <div v-if="isContentVisible" class="content-container">
-      
-      
-      <h3>Verzamelen:</h3>
-      <p>
-        Elke gebruiker krijgt een pack met 3 kaarten als ze een account aanmaken. Daarna kun je dagelijks een kaart claimen door simpelweg in te loggen, je krijgt dan de optie om te kiezen tussen 3 kaarten.
-        <br> Op <b><a class="pageLink" @click="goToCollectionPage">de collectiepagina</a></b> kun je al je eigen kaarten bekijken. Er zijn ook snelle filters die je kunt gebruiken om te filteren per type en om specifieke kaarten te vinden is er ook de zoekbalk.
-      </p>
-      
-      <h3>Codex:</h3>
-      <p>
-        In <b><a class="pageLink" @click="goToCodex">de codex</a></b> kun je alle kaarten bekijken die je hebt verzameld, alsook alle kaarten in het spel. Hier zijn dezelfde filters en zoekbalk beschikbaar als op de collectiepagina.
-        <br> Om de details van een kaart te zien kun je op de kaart klikken, deze zal dan uitvergroot worden en linksboven zie je dan alle info van de kaart.
-      </p>
-      
-      <h3>Ruilen:</h3>
-      <p>
-        Het is mogelijk om met andere studenten te ruilen, dit kan door naar <b><a class="pageLink" @click="goToTradingPage">de ruilpagina</a></b> te gaan. Hier kun je een QR-code aanmaken, of die van je medestudent scannen, als dit gebeurt is kun je de kaart(en) selecteren die je wilt ruilen.
-        <br> Je ziet ook de kaart(en) die je zult krijgen na aflopen van de ruil. Als je akkoord gaat met de ruil, kun je deze bevestigen. De kaarten worden dan automatisch naar het account van de juiste speler overgezet.
-      </p>
-      
-      <h3>Game:</h3>
-      <p>
-        Op <b><a class="pageLink" @click="goToGamePage">de gamepagina</a></b> kunnen er games tussen 2 spelers gespeeld worden, waarbij elke speler 3 kaarten kan gebruiken. De volgorde waarin de spelers beginnen, wordt bepaald door kop of munt.
-        <br> De speler die kop of munt juist had begint het gevecht en kan een van de drie opties uitvoeren: aanvallen (brengt schade toe aan de actieve kaart van de andere speler), vaardigheid gebruiken (gebruikt de vaardigheid van de actieve kaart) en wisselen (geeft je de optie om je actieve kaart te wisselen met een inactieve kaart). Daarna kan elke speler 2 acties per beurt uitvoeren.
-        <br> Als je alle kaarten van je tegenstander kunt doden, win je het gevecht en verdien je het totale aantal punten van de kaarten die je hebt verslagen.
-        <br> De verliezer krijgt ook wat punten, ze krijgen het aantal punten van de kaart met de laagste zeldzaamheid van de tegenstander.
-      </p>
-      
-      <h3>Scorebord</h3>
-      <p>
-        <b><a class="pageLink" @click="goToLeaderboardPage">Het scorebord</a></b> toont je op de hoeveelste plek je staat in de ranking, je gebruikersnaam en je score. De score is het totaal aantal punten van je kaarten bij elkaar opgeteld.
-      </p>
+      <div v-if="isContentVisible" class="content-container">
+        <h3>Verzamelen:</h3>
+        <p>
+          Elke gebruiker krijgt een pack met 3 kaarten als ze een account aanmaken. Daarna kun je dagelijks een kaart claimen door simpelweg in te loggen, je krijgt dan de optie om te kiezen tussen 3 kaarten.
+          <br> Op <b><a class="pageLink" @click="goToCollectionPage">de collectiepagina</a></b> kun je al je eigen kaarten bekijken. Er zijn ook snelle filters die je kunt gebruiken om te filteren per type en om specifieke kaarten te vinden is er ook de zoekbalk.
+        </p>
+
+        <h3>Codex:</h3>
+        <p>
+          In <b><a class="pageLink" @click="goToCodex">de codex</a></b> kun je alle kaarten bekijken die je hebt verzameld, alsook alle kaarten in het spel. Hier zijn dezelfde filters en zoekbalk beschikbaar als op de collectiepagina.
+          <br> Om de details van een kaart te zien kun je op de kaart klikken, deze zal dan uitvergroot worden en linksboven zie je dan alle info van de kaart.
+        </p>
+
+        <h3>Ruilen:</h3>
+        <p>
+          Het is mogelijk om met andere studenten te ruilen, dit kan door naar <b><a class="pageLink" @click="goToTradingPage">de ruilpagina</a></b> te gaan. Hier kun je een QR-code aanmaken, of die van je medestudent scannen, als dit gebeurt is kun je de kaart(en) selecteren die je wilt ruilen.
+          <br> Je ziet ook de kaart(en) die je zult krijgen na aflopen van de ruil. Als je akkoord gaat met de ruil, kun je deze bevestigen. De kaarten worden dan automatisch naar het account van de juiste speler overgezet.
+        </p>
+
+        <h3>Game:</h3>
+        <p>
+          Op <b><a class="pageLink" @click="goToGamePage">de gamepagina</a></b> kunnen er games tussen 2 spelers gespeeld worden, waarbij elke speler 3 kaarten kan gebruiken. De volgorde waarin de spelers beginnen, wordt bepaald door kop of munt.
+          <br> De speler die kop of munt juist had begint het gevecht en kan een van de drie opties uitvoeren: aanvallen (brengt schade toe aan de actieve kaart van de andere speler), vaardigheid gebruiken (gebruikt de vaardigheid van de actieve kaart) en wisselen (geeft je de optie om je actieve kaart te wisselen met een inactieve kaart). Daarna kan elke speler 2 acties per beurt uitvoeren.
+          <br> Als je alle kaarten van je tegenstander kunt doden, win je het gevecht en verdien je het totale aantal punten van de kaarten die je hebt verslagen.
+          <br> De verliezer krijgt ook wat punten, ze krijgen het aantal punten van de kaart met de laagste zeldzaamheid van de tegenstander.
+        </p>
+
+        <h3>Scorebord</h3>
+        <p>
+          <b><a class="pageLink" @click="goToLeaderboardPage">Het scorebord</a></b> toont je op de hoeveelste plek je staat in de ranking, je gebruikersnaam en je score. De score is het totaal aantal punten van je kaarten bij elkaar opgeteld.
+        </p>
+      </div>
     </div>
-  </div>
-      <div>
 
-
-
+    <div>
       <h2>De kaarten</h2>
       <p> Elke kaart geeft volgende info weer:</p>
       <ul class ="default list">
@@ -85,11 +88,8 @@
         <li>Onderwijs en Sociaal 🎓</li>
       </ul>
     </div>
-      <div>
 
-
-
-      
+    <div>
       <h3>Ons team:</h3>
       <ul class ="default list">
         <li>Verstraete Domien: Front- en back-end developer</li>
@@ -102,111 +102,161 @@
         <li>Degeest Bram: Robotics/ back-end developer</li>
       </ul>
       <h3>Documentatie:</h3>
-      <p>Alle documentatie is terug te vinden in onze 
+      <p>Alle documentatie is terug te vinden in onze
         <b><a class="pageLink" href="https://github.com/vives-project-xp/PX3-Campus-Games" target="_blank">github repository</a></b>.</p>
     </div>
-  </template>
-  
-  <script>
-  export default {
-        name: 'home-page',
-        data() {
+  </div>
+</template>
+
+<script>
+import { useRouter } from 'vue-router';
+import { ref, onMounted } from 'vue'; // Import ref and onMounted
+
+export default {
+  name: 'home-page',
+  setup() {
+    const router = useRouter();
+    const isContentVisible = ref(false); // Use ref for reactive data
+    const isLoggedIn = ref(false); // Reactive data for login status
+
+    const toggleContent = () => {
+      isContentVisible.value = !isContentVisible.value; // Access value with .value
+    };
+
+    const goToCodex = () => {
+      router.push('/codex');
+    };
+
+    const goToTradingPage = () => {
+      router.push('/trading');
+    };
+
+    const goToGamePage = () => {
+      router.push('/game');
+    };
+
+    const goToCollectionPage = () => {
+      router.push('/collection');
+    };
+
+    const goToLeaderboardPage = () => {
+      router.push('/leaderboard');
+    };
+
+    // Function to check login status
+    const checkLoginStatus = () => {
+      const token = localStorage.getItem('token');
+      isLoggedIn.value = !!token; // Update reactive state
+    };
+
+    // Check login status when the component is mounted
+    onMounted(() => {
+      checkLoginStatus();
+    });
+
+    const goToLogin = () => {
+        router.push('/login');
+    };
+
     return {
-      isContentVisible: false
-    } 
-  },
-
-        methods: {
-          toggleContent() {
-          this.isContentVisible = !this.isContentVisible;
-        },
-          goToCodex() {
-            this.$router.push('/codex');
-          },
-
-          goToTradingPage() {
-            this.$router.push('/trading');
-          },
-
-          goToGamePage() {
-            this.$router.push('/game');
-          },
-
-          goToCollectionPage() {
-            this.$router.push('/collection');
-          },
-
-          goToLeaderboardPage() {
-            this.$router.push('/leaderboard');
-          },
-        }
-  };
-  </script>
-
-  <style scoped>
-  
-  h2 {
-    text-align: left;
-    font-size:40px;
-    padding: 0;
-    margin: 0;
+      isContentVisible,
+      isLoggedIn, // Expose isLoggedIn to the template
+      toggleContent,
+      goToCodex,
+      goToTradingPage,
+      goToGamePage,
+      goToCollectionPage,
+      goToLeaderboardPage,
+      goToLogin // Expose goToLogin to the template
+    };
   }
+};
+</script>
 
-  p{
-    font-size:20px;
-  }
-  .logo {
-    width: 100%;
-    max-height: 50vh;
-    text-align: center;
-    margin-bottom: 2rem;
-    margin-top: 40px;
-    background-color: #fd0100; 
-    border: none; 
-    padding: 0; 
-    box-sizing: border-box; 
-    border-radius: 10px;
+<style scoped>
+/* Your existing styles */
+
+h2 {
+  text-align: left;
+  font-size: 40px;
+  padding: 0;
+  margin: 0;
+}
+
+p {
+  font-size: 20px;
+}
+
+.logo {
+  width: 100%;
+  max-height: 50vh;
+  text-align: center;
+  margin-bottom: 2rem;
+  margin-top: 40px;
+  background-color: #fd0100;
+  border: none;
+  padding: 0;
+  box-sizing: border-box;
+  border-radius: 10px;
 }
 
 .logo-image {
-    max-width: 100%;
-    height: auto;
-    display: block;
-    margin: auto;
+  max-width: 100%;
+  height: auto;
+  display: block;
+  margin: auto;
 }
-  .about-section {
-  
-  border: 2px solid grey; 
-  border-radius: 10px;    
+
+.call-to-action {
+    text-align: center;
+    margin-bottom: 20px;
+}
+
+.login-button {
+    padding: 10px 20px;
+    font-size: 5em;
+    font-weight: semi-bold;
+    background-color: white;
+    color: red;
+    border: 3px solid red;
+    border-radius: 10px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+}
+
+.about-section {
+  border: 2px solid grey;
+  border-radius: 10px;
   padding-left: 20px;
-  padding-right: 20px;          
-  margin: 20px 0;         
+  padding-right: 20px;
+  margin: 20px 0;
   background-color: lightgray;
   box-shadow: 0 5px 5px lightslategray;
 }
 
-  .pageLink {
-    color: var(--secondary-color);
-    cursor: pointer;
-    text-decoration: underline;
-  }
+.pageLink {
+  color: var(--secondary-color);
+  cursor: pointer;
+  text-decoration: underline;
+}
 
-  .default.list {
-    list-style-type: disc;
-    font-size: 18px;
-  }
-  .toggle-button {
-    padding: 15px;
-        width: 25%;
-        padding: 1rem;
-        background-color: red;
-        color: white;
-        border: none;
-        border-radius: 1rem;
-        cursor: pointer;
-        margin-top: 1rem;
-        margin-bottom: 1rem;
-        font-size: 15px;
+.default.list {
+  list-style-type: disc;
+  font-size: 18px;
+}
+
+.toggle-button {
+  padding: 15px;
+  width: 25%;
+  padding: 1rem;
+  background-color: red;
+  color: white;
+  border: none;
+  border-radius: 1rem;
+  cursor: pointer;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  font-size: 15px;
 }
 
 
@@ -216,7 +266,7 @@
   padding: 25px;
   background-color: #fff;
   margin: 20px 0;
-  box-shadow: 0 2px 10px rgba(0,0,0,0.05);
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.05);
 }
 
 h2 {
@@ -227,7 +277,8 @@ h2 {
 }
 
 h3 {
-  color: #fd0100; /* Rode kleur voor subkopjes */
+  color: #fd0100;
+  /* Rode kleur voor subkopjes */
   margin-top: 20px;
 }
 
@@ -247,4 +298,4 @@ p {
   line-height: 1.6;
   color: #555;
 }
-  </style>
+</style>
